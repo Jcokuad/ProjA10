4fdea414906987f78fa74fc516d6513d5b1f81ce
Joshua O. <156972833+Jcokuad@users.noreply.github.com>
2026-04-02 09:38:16 -0400

    Updated stack and queue header files

--- include/stackqueue/stack.hpp
+++ include/stackqueue/stack.hpp
@@ -13,21 +13,33 @@ class stack {
 
     public:
         /// return the number of elements currently in the stack
-        int size() const;
+        int size() const {
+            return data.size();
+        }
         
         /// return whether the stack is currently empty
-        bool empty() const;
+        bool empty() const {
+            return data.empty();
+        }
 
         /// return const reference to the top element of the stack
-        const T& top() const;
+        const T& top() const {
+            return data.data();
+        }
 
         /// return live (non-const) reference to the top element of the stack
-        T& top();
+        T& top() {
+            return data.back();
+        }
 
         /// add an element to the top of the stack
-        void push(const T& elem);
+        void push(const T& elem) {
+            data.push_back(elem);
+        }
 
         /// remove the top element from the stack
-        void pop();
+        void pop() {
+            data.pop_back();
+        }
 };
 }  // namespace dsac::stackqueue
